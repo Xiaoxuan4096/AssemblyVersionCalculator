65c2141cc6287adeac243e7b73bcfd0d0c241432
Xiaoxuan4096 <232077189+Xiaoxuan4096@users.noreply.github.com>
2026-06-13 12:29:00 +0800

    完成基础功能。构建1.0版本。

--- AssemblyVersionCalculator.cpp
+++ AssemblyVersionCalculator.cpp
@@ -1,20 +1,51 @@
-﻿// AssemblyVersionCalculator.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿// AssemblyVersionCalculator.cpp - Calculate assembly version by current time.
+// Written by Xiaoxuan4096.
+
+// This program is distributed under MIT License.
+// See LICENSE.txt for details.
 
 #include <iostream>
+#include <string>
+#include <limits>
+#include <chrono>
+#include <sstream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+size_t getBuild() {
+    auto begin = std::chrono::sys_days{std::chrono::year{2000} / 1 / 1}; // Year 2000, Month 1, Day 1
+    auto end = std::chrono::floor<std::chrono::days>(std::chrono::system_clock::now());
+    return static_cast<size_t>((end - begin).count());
+}
+size_t getRevision() {
+    auto begin = std::chrono::floor<std::chrono::days>(std::chrono::system_clock::now());
+    auto end = std::chrono::system_clock::now();
+    return std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() / 2;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
+void pause() {
+    std::cout << "Press Enter to continue...";
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.get();
+}
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+int main(){
+    size_t major = 0, minor = 0;
+    std::cout << "Assembly Version Calculator" << std::endl
+              << "---------------------------" << std::endl;
+
+    std::string input;
+    std::cout << "Version (e.g. 1.2): " << std::endl
+              << ">>> ";
+    std::cin >> input;
+
+    std::stringstream ss(input);
+    char dot;
+    if (!(ss >> major >> dot >> minor) || dot != '.' || ss >> dot) {
+        std::cerr << "Invalid format. Use: Major.Minor" << std::endl;
+        pause();
+        return 1;
+    }
+
+    std::cout << major << '.' << minor << '.' << getBuild() << '.' << getRevision() << std::endl;
+    pause();
+    return 0;
+}
